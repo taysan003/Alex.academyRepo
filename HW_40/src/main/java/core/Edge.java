349a5368db874ef6d0f0bccd86f9cbe6a5f5ee1e
Andrei <taysan003@gmail.com>
2018-06-09 00:08:58 -0700

    Edge

--- HW_40/src/main/java/core/Edge.java
+++ HW_40/src/main/java/core/Edge.java
@@ -1,4 +1,38 @@
 package core;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Edge {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.OFF);
+
+
+        String driverPath = "";
+        String url = "http://alex.academy/ua";
+
+        if (!System.getProperty("os.name").contains("Windows"))
+            throw new IllegalArgumentException("MS Edge is avalible only on Windows");
+
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get(url);
+        String ua = driver.findElement(By.id("id_ua")).getText();
+        System.out.println(ua);
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
 }
