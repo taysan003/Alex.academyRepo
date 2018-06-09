cb9ee9b5af9a1efd96dad1c47b9bd27282e34579
Andrei <taysan003@gmail.com>
2018-06-08 22:19:24 -0700

    first commit

---
+++ HW_40/src/main/java/core/Chrome.java
@@ -0,0 +1,55 @@
+package core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Chrome {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.OFF);
+
+        String driverPath = "";
+        String url = "http://alex.academy/ua";
+
+        if (System.getProperty("os.name").contains("Mac")){
+            driverPath = "./resources/webdrivers/mac/chromdriver";
+        } else if ( System.getProperty("os.name").contains("Windows")){
+            driverPath = "./resources/webdrivers/mac/chromdriver.exe";
+
+        } else {
+            throw new IllegalArgumentException("Unknown OS");
+        }
+
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("disable-infobars");
+        option.addArguments("--disable-notifications");
+
+        if (System.getProperty("os.name").contains("Mac")){
+            option.addArguments("-start-fullscreen");
+        } else if (System.getProperty("os.name").contains("Windows")) {
+            option.addArguments("--start-maximized");
+        }
+
+        driver = new ChromeDriver(option);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get(url);
+
+        String ua = driver.findElement(By.id("id_ua")).getText();
+        System.out.println(ua);
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+
+}
