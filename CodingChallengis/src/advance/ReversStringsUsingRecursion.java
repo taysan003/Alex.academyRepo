e01d359fe5586f4616351ec449e905c067514d49
Andrei <taysan003@gmail.com>
2018-08-07 20:19:40 -0400

    ReverseStrRecursion

---
+++ CodingChallengis/src/advance/ReversStringsUsingRecursion.java
@@ -0,0 +1,30 @@
+package advance;
+
+/*Problem: Write a program which will print reverse of string using recursion.
+Example: abcdef=>fedcba*/
+
+import java.util.Scanner;
+
+public class ReversStringsUsingRecursion {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = input.nextLine();
+        input.close();
+
+        System.out.println("Reverse string "+ reverse(str));
+    }
+
+    private static String reverse(String string){
+        if(string.length()<2)
+            return string;
+        String rever = reverse(string.substring(1))+string.charAt(0);
+        System.out.println(rever);
+
+        System.out.println(string.substring(1));
+        System.out.println(string.charAt(0));
+
+        return rever;
+    }
+}
